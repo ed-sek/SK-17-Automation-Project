bce345c94fd927698d85f568a7e43022c9707be8
Eduard S. <ed-sek@outlook.com>
2025-03-09 22:47:31 +0200

    Implement screenshot functionality and add screenshot folder

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,10 +2,13 @@ package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -14,7 +17,10 @@ import org.testng.annotations.BeforeSuite;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public abstract class TestBase {
@@ -22,12 +28,14 @@ public abstract class TestBase {
     public static final String TEST_RESOURCES_DIR = "src\\test\\resources\\";
     public static final String REPORTS_DIR = TEST_RESOURCES_DIR.concat("reports\\");
     public static final String DOWNLOAD_DIR = TEST_RESOURCES_DIR.concat("download\\");
+    public static final String SCREENSHOT_DIR = TEST_RESOURCES_DIR.concat("screenshot\\");
 
     private WebDriver webDriver;
 
     @BeforeSuite
     protected void setupTestSuite() {
         cleanDirectory(REPORTS_DIR);
+        cleanDirectory(SCREENSHOT_DIR);
         WebDriverManager.chromedriver().setup();
         WebDriverManager.firefoxdriver().setup();
         WebDriverManager.edgedriver().setup();
@@ -42,12 +50,13 @@ public abstract class TestBase {
     }
 
     @AfterMethod
-    protected void tearDownTest() {
+    protected void tearDownTest(ITestResult testResult) {
+        takeScreenshot(testResult);
         quitDriver();
     }
 
     @AfterSuite
-    public void deleteDownloadedFiles() {
+    protected void deleteDownloadedFiles() {
         cleanDirectory(DOWNLOAD_DIR);
     }
 
@@ -92,4 +101,36 @@ public abstract class TestBase {
         chromeOptions.setExperimentalOption("prefs", prefs);
         return chromeOptions;
     }
+
+    private void takeScreenshot(ITestResult testResult) {
+        if (ITestResult.FAILURE == testResult.getStatus()) {
+            try {
+                // Cast webDriver to TakesScreenshot to take a screenshot
+                TakesScreenshot takesScreenshot = (TakesScreenshot) webDriver;
+                File screenshot = takesScreenshot.getScreenshotAs(OutputType.FILE);
+                String testName = testResult.getName();
+
+                // Append any test parameters to the test name (if they are not null or empty)
+                for (Object param : testResult.getParameters()) {
+                    if (param != null && !param.toString().isEmpty()) {
+                        testName = testName + "_" + param;
+                    }
+                }
+
+                // Get the current timestamp to make the filename unique
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+                String timestamp = LocalDateTime.now().format(formatter);
+
+                // Combine the test name and timestamp to create a unique filename
+                String fileName = testName + "_" + timestamp + ".jpg";
+
+                // Save the screenshot to the specified directory with the timestamped test name
+                FileUtils.copyFile(screenshot, new File(SCREENSHOT_DIR.concat(fileName)));
+
+            } catch (IOException e) {
+                System.out.println("Unable to create a screenshot file: " + e.getMessage());
+            }
+        }
+    }
+
 }
